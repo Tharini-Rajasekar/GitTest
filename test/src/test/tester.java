48d3a92b9259ff01ec006c043b2f7dccd86b75a7
tharini-pt7291 <tharini-pt7291@tharini-pt7292>
2024-05-06 15:44:39 +0530

    commit 4

--- test/src/test/tester.java
+++ test/src/test/tester.java
@@ -3,10 +3,10 @@ package test;
 public class tester {
 
 	public static void main(String[] args) {
-		System.out.println("hi");
-		System.out.println("hello");
-		System.out.println("bye");
 		System.out.println("salut");
+		System.out.println("hello");
+		System.out.println("bonjor");
+		System.out.println("revior");
 
 	}
 
