048a74e5f0fb533e0295ae4678ec93c4acb85c96
tharini-pt7291 <tharini-pt7291@tharini-pt7292>
2024-05-07 17:04:12 +0530

    commit 1 feature

--- test/src/test/tester.java
+++ test/src/test/tester.java
@@ -6,7 +6,7 @@ public class tester {
 		System.out.println("sum of 2 and 5 is "+(2+5));
 		System.out.println("sum of 5 and 5 is "+(5+5));
 		System.out.println("sum of 10 and 5 is "+(10+5));
-		System.out.println("hello world");
+		System.out.println("Hello world");
 
 	}
 
