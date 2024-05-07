072ebe77feed810ca0486e05f295efd2ddd2c8fb
tharini-pt7291 <tharini-pt7291@tharini-pt7292>
2024-05-07 17:10:32 +0530

    commit 1 master

--- test/src/test/tester.java
+++ test/src/test/tester.java
@@ -3,10 +3,9 @@ package test;
 public class tester {
 
 	public static void main(String[] args) {
-		System.out.println("hi salut");
-		System.out.println("hello");
-		System.out.println("byee");
-		System.out.println("revior");
+		System.out.println("sum of 2 and 5 is "+(2+5));
+		System.out.println("sum of 5 and 5 is "+(5+5));
+		
 
 	}
 
