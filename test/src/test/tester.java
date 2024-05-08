8aefcedfab5a8882c9b5e49df961eafb524eb6b4
tharini-pt7291 <tharini-pt7291@tharini-pt7292>
2024-05-08 10:58:34 +0530

    change 2

--- test/src/test/tester.java
+++ test/src/test/tester.java
@@ -5,8 +5,7 @@ public class tester {
 	public static void main(String[] args) {
 		System.out.println("sum of 2 and 5 is "+(2+5));
 		System.out.println("sum of 5 and 5 is "+(5+5));
-		System.out.println("product of 10 and 5 is "+(10*5));		
-		System.out.println("sum of 10 and 5 is "+(10+5));
+		System.out.println("hello");
 		
 
 	}
