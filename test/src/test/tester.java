1d248421cfc1f2e25b61a3e64f3cd3a702e8486e
tharini-pt7291 <tharini-pt7291@tharini-pt7292>
2024-05-08 11:34:59 +0530

    cp 1

--- test/src/test/tester.java
+++ test/src/test/tester.java
@@ -5,7 +5,7 @@ public class tester {
 	public static void main(String[] args) {
 		System.out.println("sum of 2 and 5 is "+(2+5));
 		System.out.println("sum of 5 and 5 is "+(5+5));
-		System.out.println("hiiii");
+		System.out.println("hello");
 		
 
 	}
